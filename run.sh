533684f20656316faf5f36972a31465f386092de
steelburn <steelburn@gmail.com>
2020-06-16 10:30:35 +0800

    Changed run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
-cd eLeaveWooCommerceCore
+cd /app
 
 # Commenting out the following two due to changing to webhook instead of constant checking
 # npm i
 # watch -n 120 'git stash && git pull' &
 #npm run start:dev &
 # sleep 120
-
-# Removed on 19 September 2019 - documentation & testing now handled by steelburn/eleavedocs docker image.
-# ../httpd-run.sh &
 
 npm run start
